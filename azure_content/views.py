840afccef1a964d97b54d70c5fd1563ba79f9819
Cezar Bican <109511613+cezarbican@users.noreply.github.com>
2023-12-04 15:26:50 +0200

    Update views.py

--- azure_content/views.py
+++ azure_content/views.py
@@ -77,12 +77,12 @@ def Login(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-            return redirect('/')
-        else:
-            messages.info(request, 'Username or Password is incorrect!')
+        login(request, user)
+        # if user is not None:
+        #     login(request, user)
+        #     return redirect('/')
+        # else:
+        #     messages.info(request, 'Username or Password is incorrect!')
 
     context = {'title' : 'Login'}
     return render(request, 'azure_content/Login.html', context)

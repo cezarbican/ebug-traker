c97c4f1b1302b9679af5ee1d62e9c740e500fcab
Cezar Bican <cezarbican@gmail.com>
2023-12-04 12:59:06 +0200

    adding rest framework

--- azure_project/deployment.py
+++ azure_project/deployment.py
@@ -37,3 +37,11 @@ DATABASES = {
         'PASSWORD': conn_str_params['password'],
     }
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    # Other DRF settings...
+}
